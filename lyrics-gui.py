105e8fd1a0d211b34e8ab9809f510c89c1ce34b2
sirken <sirken@users.noreply.github.com>
2023-11-09 12:52:37 -0700

    Update to newer lyrics-extractor

--- lyrics-gui.py
+++ lyrics-gui.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 
-from lyrics_extractor import Song_Lyrics
+from lyrics_extractor import SongLyrics
 import sys
 import tkinter as tk
 
 # API and Search engine
-extract_lyrics = Song_Lyrics("API-KEY", "ENGINE-ID")
+extract_lyrics = SongLyrics("API-KEY", "ENGINE-ID")
 
 query = ""
 
 # Check for cli arguments, [0] is always filename
 if len(sys.argv) > 1:
-	args = sys.argv
-	for a in args[1:]:
-		query = query + " " + a
+    args = sys.argv
+    for a in args[1:]:
+        query = query + " " + a
 # Otherwise, prompt for input
 else:
     query = input("Lyrics search: ")
 
-song_title, song_lyrics = extract_lyrics.get_lyrics(query)
+song = extract_lyrics.get_lyrics(query)
 
-#print(song_title)
-#print(song_lyrics)
+# print(song)
 
 # Open the main window & start the Tcl interpreter
 root = tk.Tk()
@@ -43,9 +42,9 @@ viewer.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 scrollbar.config(command=viewer.yview)
 
 # Get the file name from the command line
-#fname = sys.argv[1]
+# fname = sys.argv[1]
 
 # Read the text file and add its contents to the Text widget
-viewer.insert(tk.END, song_title + "\r\n\r\n" + song_lyrics)
+viewer.insert(tk.END, song['title'] + "\n\n" + song['lyrics'])
 
 root.mainloop()
