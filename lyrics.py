1b63b938bc7a9a9d27620686bb1d7283df706476
sirken <sirken@users.noreply.github.com>
2023-11-09 12:52:53 -0700

    Pipe to less, update to newer lyrics-extractor

--- lyrics.py
+++ lyrics.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 
-from lyrics_extractor import Song_Lyrics
+from lyrics_extractor import SongLyrics
+from subprocess import Popen, PIPE
 import sys
 
 # API and Search engine
-extract_lyrics = Song_Lyrics("API-KEY", "ENGINE-ID")
+extract_lyrics = SongLyrics("API-KEY", "ENGINE-ID")
 
 query = ""
 
 # Check for cli arguments, [0] is always filename
 if len(sys.argv) > 1:
-	args = sys.argv
-	for a in args[1:]:
-		query = query + " " + a
+    args = sys.argv
+    for a in args[1:]:
+        query = query + " " + a
 # Otherwise, prompt for input
 else:
     query = input("Lyrics search: ")
 
-song_title, song_lyrics = extract_lyrics.get_lyrics(query)
-
-print(song_title)
-print(song_lyrics)
+try:
+    song = extract_lyrics.get_lyrics(query)
+    final_lyrics = song['title'] + '\n' + song['lyrics']
+    ps = Popen(("echo", final_lyrics), stdout=PIPE)
+    output = Popen(('less'), stdin=ps.stdout)
+    output.communicate()
+except Exception as e:
+    print(e)
